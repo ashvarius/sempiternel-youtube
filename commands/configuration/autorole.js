eb41a897dba9664574ace8ffee0d7c467e464513
Alexandre Hallaine <hallainealexandre@gmail.com>
2021-02-15 22:33:53 +0100

    update: autorole

--- commands/configuration/autorole.js
+++ commands/configuration/autorole.js
@@ -1,69 +1,85 @@
 module.exports = {
 	name: 'autorole',
-	aliases: [],
 	description: 'description_autorole',
-	command: command => {
-		if (!command.args.length || !['add', 'remove', 'restore'].includes(command.args[0].toLowerCase())) {
-			const embed = command.message.client.utils.createEmbed();
-			embed.addField(`${command.prefix}${command.command} add <role>`, command.message.client.utils.getMessage(command.message.channel, 'autorole_help_add'));
-			embed.addField(`${command.prefix}${command.command} remove <role>`, command.message.client.utils.getMessage(command.message.channel, 'autorole_help_remove'));
-			embed.addField(`${command.prefix}${command.command} restore <on/off>`, command.message.client.utils.getMessage(command.message.channel, 'autorole_help_restore'));
-			command.message.client.utils.sendEmbed(command.message.channel, embed);
-			return;
+	options: [
+		{
+			type: 1,
+			name: 'add',
+			description: 'autorole_help_add',
+			options: [
+				{
+					type: 8,
+					name: 'role',
+					description: 'autorole_help_add',
+					required: true
+				}
+			]
+		},
+		{
+			type: 1,
+			name: 'remove',
+			description: 'autorole_help_remove',
+			options: [
+				{
+					type: 8,
+					name: 'role',
+					description: 'autorole_help_remove',
+					required: true
+				}
+			]
+		},
+		{
+			type: 1,
+			name: 'restore',
+			description: 'autorole_help_restore',
+			options: [
+				{
+					type: 5,
+					name: 'boolean',
+					description: 'autorole_help_restore',
+					required: true
+				}
+			]
 		}
-		const cmd = command.args[0].toLowerCase();
-		if (cmd == 'restore') {
-			const option = command.args[1] && command.args[1].toLowerCase();
-			if (!['on', 'off'].includes(option)) {
-				const embed = command.message.client.utils.createEmbed();
-				embed.addField(`${command.prefix}${command.command} ${command.args[0]} <on/off>`, command.message.client.utils.getMessage(command.message.channel, 'autorole_help_restore'));
-				command.message.client.utils.sendEmbed(command.message.channel, embed);
-				return;
-			}
-			const guildData = command.message.client.utils.readFile(`guilds/${command.message.guild.id}.json`);
-			if (!guildData.autorole)
-				guildData.autorole = {};
-			guildData.autorole.restore = option == 'on';
-			command.message.client.utils.savFile(`guilds/${command.message.guild.id}.json`, guildData);
-			command.message.client.utils.sendMessage(command.message.channel, 'autorole_restore', { option });
-		} else {
-			if (Array.from(command.message.mentions.roles.values()).length != 1) {
-				const embed = command.message.client.utils.createEmbed();
-				embed.addField(`${command.prefix}${command.command} ${cmd} <role>`, command.message.client.utils.getMessage(command.message.channel, `autorole_help_${cmd}`));
-				command.message.client.utils.sendEmbed(command.message.channel, embed);
-				return;
-			}
-			const role = Array.from(command.message.mentions.roles.values())[0];
-			const guildData = command.message.client.utils.readFile(`guilds/${command.message.guild.id}.json`);
-			if (!guildData.autorole)
-				guildData.autorole = {};
+	],
+	command: async object => {
+		const command = object.options[0].name;
+		const guildData = await object.client.utils.readFile(object.client.utils.docRef.collection('guild').doc(object.guild.id));
+		if (!guildData.autorole)
+			guildData.autorole = {};
+		let role;
+		if (command == 'restore')
+			guildData.autorole.restore = object.options[0].options[0].value;
+		else {
+			role = await object.guild.roles.fetch(object.options[0].options[0].value);
 			if (!guildData.autorole.roles)
 				guildData.autorole.roles = [];
-			if (cmd == 'add') {
+			if (command == 'add') {
 				if (guildData.autorole.roles.indexOf(role.id) == -1)
 					guildData.autorole.roles.push(role.id);
 			} else if (!guildData.autorole.roles.indexOf(role.id) != -1)
 				guildData.autorole.roles.splice(guildData.autorole.roles.indexOf(role.id), 1);
-			command.message.client.utils.savFile(`guilds/${command.message.guild.id}.json`, guildData);
-			if (cmd == 'add')
-				command.message.client.utils.sendMessage(command.message.channel, 'autorole_add', { role });
-			else
-				command.message.client.utils.sendMessage(command.message.channel, 'autorole_remove', { role });
 		}
+		object.client.utils.savFile(object.client.utils.docRef.collection('guild').doc(object.guild.id), guildData);
+		if (command == 'restore')
+			return object.client.utils.getMessage(object.channel, 'autorole_restore', { option: object.options[0].options[0].value });
+		if (command == 'add')
+			return object.client.utils.getMessage(object.channel, 'autorole_add', { role });
+		return object.client.utils.getMessage(object.channel, 'autorole_remove', { role });
 	},
-	permission: (message) => {
-		if (!message.member.hasPermission('ADMINISTRATOR'))
+	permission: object => {
+		if (!object.member.hasPermission('ADMINISTRATOR'))
 			return false;
 		return true;
 	},
-	guildMemberAdd: member => {
+	guildMemberAdd: async member => {
 		if (!member.guild.me.hasPermission('MANAGE_ROLES'))
 			return;
-		const guildData = member.client.utils.readFile(`guilds/${member.guild.id}.json`);
+		const guildData = await member.client.utils.readFile(member.client.utils.docRef.collection('guild').doc(member.guild.id));
 		if (!guildData.autorole)
 			return;
 		if (guildData.autorole.restore) {
-			const userData = member.client.utils.readFile(`users/${member.id}.json`);
+			const userData = await member.client.utils.readFile(member.client.utils.docRef.collection('users').doc(member.id));
 			if (!userData.autorole)
 				userData.autorole = {};
 			if (userData.autorole[member.guild.id]) {
@@ -83,11 +99,11 @@ module.exports = {
 				member.roles.add(role);
 		}
 	},
-	guildMemberRemove: member => {
-		const guildData = member.client.utils.readFile(`guilds/${member.guild.id}.json`);
+	guildMemberRemove: async member => {
+		const guildData = await member.client.utils.readFile(member.client.utils.docRef.collection('guild').doc(member.guild.id));
 		if (!(guildData.autorole && guildData.autorole.restore))
 			return;
-		const userData = member.client.utils.readFile(`users/${member.id}.json`);
+		const userData = await member.client.utils.readFile(member.client.utils.docRef.collection('users').doc(member.id));
 		if (!userData.autorole)
 			userData.autorole = {};
 		userData.autorole[member.guild.id] = [];
@@ -95,6 +111,6 @@ module.exports = {
 		for (const role of member.roles.cache.values())
 			if (role != everyone)
 				userData.autorole[member.guild.id].push(role.id);
-		member.client.utils.savFile(`users/${member.id}.json`, userData);
+		member.client.utils.savFile(member.client.utils.docRef.collection('users').doc(member.id), userData);
 	}
 };
