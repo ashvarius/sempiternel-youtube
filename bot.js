573270d4753866dcb97f4ca06bad1ee95d1055f9
Alexandre Hallaine <hallainealexandre@gmail.com>
2021-09-22 09:29:31 +0200

    add: sharding

---
+++ bot.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const winston = require('winston');
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
+	ws: {
+		properties: {
+			$os: 'Android',
+			$browser: 'Discord Android',
+		},
+	},
+});
+
+client.logger = winston.createLogger({
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({ filename: 'log' }),
+	],
+	format: winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) client.once(event.name, (...args) => event.execute(...args));
+	else client.on(event.name, (...args) => event.execute(...args));
+}
+
+client.on('debug', m => client.logger.log('debug', m));
+client.on('warn', m => client.logger.log('warn', m));
+client.on('error', m => client.logger.log('error', m));
+
+client.on('shardReady', id => client.user.setPresence({
+	activities: [{
+		name: `shard ${id}`,
+		type: 'WATCHING',
+	}],
+}));
+
+process.on('uncaughtException', error => client.logger.log('error', error));
+
+process.on('exit', () => client.destroy());
+
+process.on('SIGINT', () => process.exit());
+process.on('SIGUSR1', () => process.exit());
+process.on('SIGUSR2', () => process.exit());
+
+client.login(token);
--- index.js
+++ index.js
@@ -1,55 +1,8 @@
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { ShardingManager } = require('discord.js');
 const { token } = require('./config.json');
-const winston = require('winston');
 
-const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
-	ws: {
-		properties: {
-			$os: 'Android',
-			$browser: 'Discord Android',
-		},
-	},
-});
+const manager = new ShardingManager('./bot.js', { token });
 
-client.logger = winston.createLogger({
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({ filename: 'log' }),
-	],
-	format: winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
-});
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-client.on('debug', m => client.logger.log('debug', m));
-client.on('warn', m => client.logger.log('warn', m));
-client.on('error', m => client.logger.log('error', m));
-
-process.on('uncaughtException', error => client.logger.log('error', error));
-
-process.on('exit', () => client.destroy());
-
-process.on('SIGINT', () => process.exit());
-process.on('SIGUSR1', () => process.exit());
-process.on('SIGUSR2', () => process.exit());
-
-client.login(token);
+manager.spawn();
